08b78a02af847b9ac1dbf515ea145dae48952a14
jsandmann <45406091+jsandmann@users.noreply.github.com>
2019-12-02 17:46:22 -0800

    updated redirect uris

--- application.py
+++ application.py
@@ -16,15 +16,16 @@ def getlikedsongs():
   #Get authorization code
   code=request.args.get('code')
   url = "https://accounts.spotify.com/api/token"
-  payload = "grant_type=authorization_code&code={}&redirect_uri=http://optimizedliving.azurewebsites.net/callback&client_id=f703f57d46f34a7e9fffc4df3b4a9994&client_secret=1fe906d84e3e4a0db0812483390bbd8b".format(code)
+  payload = "grant_type=authorization_code&code={}&redirect_uri=https://optimizedliving.azurewebsites.net/callback&client_id=f703f57d46f34a7e9fffc4df3b4a9994&client_secret=1fe906d84e3e4a0db0812483390bbd8b".format(code)
   headers = {'Content-Type': "application/x-www-form-urlencoded"}
   response = requests.request("POST", url, data=payload, headers=headers)
   parsed_json=json.loads(response.text)
+  print(parsed_json)
   token_type=parsed_json['token_type']
   scope=parsed_json['scope']
   expires_in=parsed_json['expires_in']
   access_token=parsed_json['access_token']
-
+  return render_template('success.html')
   #Get Tracks
 #   Tracks = []
 
@@ -45,4 +46,3 @@ def getlikedsongs():
 #   print("We have", len(Tracks), "total results")
 #   with open('music.json', 'w', encoding='utf-8') as f:
 #       json.dump(Tracks, f, ensure_ascii=False, indent=4)
-  return render_template('success.html')
