560f8d2a0ba10d3b70fdd867dac06fa86f3963f9
jsandmann <45406091+jsandmann@users.noreply.github.com>
2020-03-02 20:27:44 -0800

    app run

--- application.py
+++ application.py
@@ -272,3 +272,6 @@ def pretty_print_response(response):
 
 def format_error(e):
   return {'error': {'display_message': e.display_message, 'error_code': e.code, 'error_type': e.type, 'error_message': e.message } }
+
+if __name__ == '__main__':
+    app.run()
